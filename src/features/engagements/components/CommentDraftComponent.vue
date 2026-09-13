b1ee7c966df04217d3304955d1d94b76b6b20cd3
Obieze <obieze6@yahoo.com>
2026-09-13 01:09:40 +0100

    style: update stylesheet import in CommentDraftComponent for consistency

--- src/features/engagements/components/CommentDraftComponent.vue
+++ src/features/engagements/components/CommentDraftComponent.vue
@@ -155,5 +155,5 @@ async function handleModalUpdate(type: string, title: string) {
 </template>
 
 <style lang="less" scoped>
-@import "@/assets/css/comment-lists.less";
+@import "@/assets/css/comment-card.less";
 </style>
